5dd67863dc1a3333ef5899796e2ce9a5b62c62b1
24zeroes <vdmbat@gmail.com>
2019-12-04 01:03:20 +0300

    old velocity in combat mode

--- VelocityBuilder.cs
+++ VelocityBuilder.cs
@@ -4,14 +4,7 @@ namespace AiCup2019{
         public static double Build(Strategy strategy, Unit player, Vec2Double target, Tile[][] tiles){
             if (strategy.Type == StrategyType.Kill && (player.OnLadder(tiles) == false && player.OnJumper(tiles) == false))
             {
-                if (player.Position.X > target.X)
-                {
-                    return  - 7;
-                }
-                else
-                {
-                    return  + 7;
-                }
+                return target.X - player.Position.X;
                 
             }
                 
