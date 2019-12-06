9c6034eab75345555834baf52e6f24689cae8fc1
24zeroes <vdmbat@gmail.com>
2019-12-07 01:19:44 +0300

    smart shooting

--- UnitExtention.cs
+++ UnitExtention.cs
@@ -1,5 +1,6 @@
 using AiCup2019.Model;
 using System.Linq;
+using System;
 
 namespace AiCup2019{
     public static class UnitExtention{
@@ -25,17 +26,34 @@ namespace AiCup2019{
                 level[(int)player.Position.X + 1][(int)player.Position.Y] == Tile.Platform;
         }
         public static bool CanShoot(this Unit player, Unit target, Tile[][] tiles, Nearest nearest){
-            if (player.Weapon.HasValue && player.Weapon.Value.Typ == WeaponType.RocketLauncher)
-            {
-                if (tiles[(int)(player.Position.X + 1)][(int)(player.Position.Y)] == Tile.Wall)
-                    return false;
+            var tan = ((nearest.Enemy.Value.Position.Y - player.Position.Y) / (nearest.Enemy.Value.Position.X - player.Position.X));
             
-                if (tiles[(int)(player.Position.X - 1)][(int)(player.Position.Y)] == Tile.Wall)
-                    return false;
-            }
+            var isRightSide = nearest.Enemy.Value.Position.X < player.Position.X;
+            var increment = isRightSide ? -0.3 : 0.3;
 
-            if (Helpers.DistanceSqr(player.Position, target.Position) > 300 && nearest.Enemy.Value.Health > 30)
+            double x1 = player.Position.X;
+            double y1 = player.Position.Y + 1;
+            double x2 = nearest.Enemy.Value.Position.X;
+            double y2 = nearest.Enemy.Value.Position.Y + 1;
+            double x = x1;
+            double y = y1;
+
+            try
+            {
+                while ((int)x != (int)(x2))
+                {
+                    x = x + increment;
+                    y = (x * y2 - x * y1 - x1 * y2 + x2 * y1) / (x2 - x1);
+                    if ((int)x == (int)(x2) && ((int)y == (int)y2 || (int)y == (int)(y2 + 1) || (int)y == (int)(y2 - 1)))
+                        break;
+
+                    if (tiles[(int)x][(int)y] == Tile.Wall)
+                        return false;
+                }
+            }
+            catch(Exception e){
                 return false;
+            }
 
             return true;
         }
